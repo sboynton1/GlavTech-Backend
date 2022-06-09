ffb0c0895381b8991bb0fc11fcac6f4b6d1555c7
sboynton1 <sboynton@purdue.edu>
2022-06-08 23:32:48 -0400

    Login Authentication

---
+++ src/main/java/tech/GlavTech/SD2022/register/RegistrationController.java
@@ -0,0 +1,22 @@
+package tech.GlavTech.SD2022.register;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+
+    private RegistrationService registrationService;
+
+    @PostMapping
+    public ResponseEntity<String> register(@RequestBody RegistrationRequest RR){
+        System.out.println("Received Request");
+        return registrationService.register(RR);
+    }
+}
